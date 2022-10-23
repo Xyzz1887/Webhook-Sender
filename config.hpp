e3804509d75968a457d69a193f127fe3ad529f1f
Xyzz1887 <107305685+Xyzz1887@users.noreply.github.com>
2022-10-23 19:22:55 +0000

    Delete config.hpp

--- config.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <string>
-
-namespace cfg{
-    std::string jsonmsg = "{\"embeds\": [{\"title\": \"EmbedTitle\", \"description\": \"Embed description\", \"url\": \"https://github.com/Xyzz1887\", \"image\": {\"url\": \"https://imageurl.png\"}}]}";
-    int sleeptimer = 2;
-}
